d6f13d48daa5e6a05b7fb774c817b0d730bb6001
Jas Menjivar <mm0588032021@unab.edu.sv>
2021-10-18 13:19:06 -0600

    Create ClsREstudiantes.cs

---
+++ ClsREstudiantes.cs
@@ -0,0 +1,65 @@
+﻿using Estudiantes.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Estudiantes.Repository
+{
+    class ClsREstudiantes : IEstudiante 
+    {
+        public void Create(estudiante estudiante)
+        {
+            using (EstudiantessEntities conexionBD = new EstudiantessEntities())
+            {
+
+                try
+                {
+
+                    
+
+                    conexionBD.estudiante.Add(estudiante);
+                    conexionBD.SaveChanges();
+                    MessageBox.Show("Guardado");
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error en el ingreso a la base de datos" + ex);
+                }
+
+
+
+
+            }
+
+
+
+        }
+
+        public void Delete(estudiante estudiante)
+        {
+           
+        }
+
+        public List<estudiante> ListOfDataEstudiante()
+        {
+
+            using (EstudiantessEntities conexionBD = new EstudiantessEntities())
+            {
+
+                List<estudiante>ListOfdata= conexionBD.estudiante.ToList();
+                //select*from  estudiante
+
+
+                return ListOfdata;
+            }
+        }
+        
+    }
+}
+
+
+
